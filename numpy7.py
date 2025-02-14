cf53bffeb9c6ffa13d6b70303c7cbfd1a0742276
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:59:03 -0600

    Add files via upload

---
+++ numpy7.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+
+# In[9]:
+
+
+print(arr)
+
+
+# In[10]:
+
+
+my_array = arr.reshape(3,4)
+
+
+# In[11]:
+
+
+print(my_array)
+
+
+# In[ ]:
+
+
+
+
