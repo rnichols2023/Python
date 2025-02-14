d77ba81554df1d46cf5f48dc5ba5b2d4912cbf66
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 08:55:39 -0600

    Add files via upload
    List: Remove/Append

---
+++ python9.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+food = ["apple" , "bread" , "orange" , "soup"]
+
+
+# In[2]:
+
+
+food.remove("soup")
+
+
+# In[3]:
+
+
+food.append("crackers")
+
+
+# In[4]:
+
+
+food.append("milk")
+
+
+# In[5]:
+
+
+print(food)
+
+
+# In[ ]:
+
+
+
+
