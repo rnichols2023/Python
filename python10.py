beba43a512e336a92ea68f6c26190eae9e9237a4
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 08:56:12 -0600

    Add files via upload
    Concatenate Two Lists

---
+++ python10.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+courses = ["English" , "Statistics" , "Leadership" , "History"]
+
+
+# In[10]:
+
+
+substitutes = ["Literature" , "Biology" , "Humanities" , "Education"]
+
+
+# In[11]:
+
+
+schedule = (courses + substitutes) 
+
+
+# In[12]:
+
+
+print(schedule)
+
+
+# In[ ]:
+
+
+
+
