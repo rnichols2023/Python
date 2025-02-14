dad43bef0f88e753e01953594bef2b69815f2dba
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 08:53:16 -0600

    Add files via upload
    Reversing a String

---
+++ python5.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+my_string = "Belhaven University!"
+
+
+# In[40]:
+
+
+E = my_string[::-1]
+
+
+# In[41]:
+
+
+print(E)
+
+
+# In[ ]:
+
+
+
+
