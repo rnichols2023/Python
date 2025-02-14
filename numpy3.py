5e7f687ca6b04e7bf409a4d103c2b86e19e91d97
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:58:02 -0600

    Add files via upload

---
+++ numpy3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import numpy as np
+
+
+# In[11]:
+
+
+arr1 = np.array([[[6, 7, 9, 2], [5, 12, 2, 10], [14, 10, 13, 15]],  [[3, 6, 5, 4], [14, 9, 8, 13], [7, 11, 1, 12]]])
+
+
+# In[12]:
+
+
+print(arr1)
+
+
+# In[13]:
+
+
+data = arr1[1, 2, 0]
+
+
+# In[14]:
+
+
+print(data)
+
+
+# In[ ]:
+
+
+
+
