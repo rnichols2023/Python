cbe8fbd5a914686d3fdc883409ad4140fd50441f
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:58:18 -0600

    Add files via upload

---
+++ numpy4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+from numpy import random
+
+
+# In[3]:
+
+
+random_arr = np.random.rand(3,5)
+
+
+# In[4]:
+
+
+print(random_arr)
+
+
+# In[ ]:
+
+
+
+
