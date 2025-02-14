4ddcfe3ead0d1ac162e8336ec8f16da0acdd10ba
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:58:34 -0600

    Add files via upload

---
+++ numpy5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr = np.array([1, 5, 0, -1, 10])
+
+
+# In[11]:
+
+
+print(arr)
+
+
+# In[12]:
+
+
+boolean_arr = arr.astype(bool)
+
+
+# In[13]:
+
+
+print(boolean_arr)
+
+
+# In[ ]:
+
+
+
+
