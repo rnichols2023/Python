358c7db9104db0d9422dd48d1f396ebcffb1d44c
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:58:48 -0600

    Add files via upload

---
+++ numpy6.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+person_1 = {"Sandwich" : "Hamburger", "Side" : "Fries" , "Drink" : "Diet Coke"}
+
+
+# In[6]:
+
+
+person_2 = {"Sandwich" : "Chicken Fingers" , "Side" : "Tater Tots" , "Drink" : "Coke"}
+
+
+# In[7]:
+
+
+person_3 = {"Sandwich" : "Ham & Cheese" , "Side" : "Salad" , "Drink" : "Water"}
+
+
+# In[8]:
+
+
+my_orders = {"Mike" : person_1,
+             "Oliver" : person_2,
+             "Tammy" : person_3}
+
+
+# In[9]:
+
+
+print(my_orders)
+
+
+# In[ ]:
+
+
+
+
