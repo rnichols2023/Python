f9e9aefeb9a95459b35e9e89f945762b824580f1
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:59:30 -0600

    Add files via upload

---
+++ numpy9.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+import numpy as np
+
+
+# In[15]:
+
+
+my_array = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10],  [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]])
+
+
+# In[16]:
+
+
+print(my_array)
+
+
+# In[20]:
+
+
+my_array.max()
+
+
+# In[21]:
+
+
+my_array.mean()
+
+
+# In[ ]:
+
+
+
+
