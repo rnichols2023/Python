87b8012e40d170fc23f56874b95fd01081d105fb
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 09:59:43 -0600

    Add files via upload

---
+++ numpy10.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[129]:
+
+
+import numpy as np
+
+
+# In[130]:
+
+
+temp_readings = np.array([[58, 65, 44], [57, 65, 37], [29, 45, 30], [32, 59, 31], [33, 60, 40]])
+
+
+# In[131]:
+
+
+print(temp_readings)
+
+
+# In[132]:
+
+
+print("Min:", (np.min(temp_readings)))
+
+
+# In[133]:
+
+
+print("Max:", (np.max(temp_readings)))
+
+
+# In[ ]:
+
+
+
+
