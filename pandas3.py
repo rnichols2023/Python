d29defeec9636d17dc4d6f81a12f5fa262879a0d
Rachel Nichols <rnichols2023@gmail.com>
2025-02-14 10:38:22 -0600

    Add files via upload

---
+++ pandas3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+tips = pd.read_csv("tips.csv")
+
+
+# In[3]:
+
+
+print(tips)
+
+
+# In[4]:
+
+
+print(tips.tail(5))
+
+
+# In[ ]:
+
+
+
+
